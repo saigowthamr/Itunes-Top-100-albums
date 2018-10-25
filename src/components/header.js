93dd4aac326016851fc90fae7c20c39960abd255
saigowthamr <saigowthamr@gmail.com>
2018-10-25 09:34:35 +0530

    url added

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import './header.css';
 
 export default () => (
     <header className="header">
-        <a href="/" style={{
+        <a href="https://saigowthamr.github.io/Itunes-Top-100-albums/" style={{
             textDecoration: "none",
             color: "white", borderBottom: "1px solid red"
         }}>Top 100 Albums from Itunes</a>
